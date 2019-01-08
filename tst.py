dbb7e746d1c6e58901b190db98ff696cfd44fbf2
Samuel Cardonis <o528060707@gmail.com>
2019-01-08 22:37:32 +0200

    first commit

---
+++ tst.py
@@ -0,0 +1,12 @@
+mylist = []
+mylist.append(10)
+mylist.append(20)
+mylist.append(30)
+print("here is the list with 1 by 1:")
+print(mylist[0]) # prints 1
+print(mylist[1]) # prints 2
+print(mylist[2]) # prints 3
+print("here is the list with for loop:")
+# prints out 1,2,3
+for x in mylist:
+    print(x)
---
+++ tst2.py
@@ -0,0 +1,26 @@
+print (8+9+9)
+print ("bye")
+print ('Hello Wold')
+name = input("What's your name? ")
+print("Nice to meet you " + name + "!")
+age = input("Your age? ")
+print("So, you are already " + str(age) + " years old, " + name + "!")
+
+a=int(input("num1?"))
+b=int(input("num2?"))
+c=int(input("num3?"))
+cost=a+b+c
+print("sum is")
+print(cost)
+
+
+d=input("4 digit string ?")
+print("sum of numbers:")
+print(  int(d[0]))
+print(  int(d[1]))
+print(  int(d[2]))
+print(  int(d[3]))
+
+cost =  int(d[0]) + int(d[1]) + int(d[2]) + int(d[3])
+print("sum is")
+print(cost)
